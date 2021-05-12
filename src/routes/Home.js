07ba961c8858fdad7af5abeff0ddee35620598fd
coderB <flack319@gmail.com>
2021-05-12 10:27:33 +0900

    show the all of the mind

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DBService } from "fbase";
 
 const Home = ({ userObj }) => {
     const [mind, setMind] = useState("");
+    const [minds, setMinds] = useState([]);
 
+    const getMinds = async () => {
+        const loadMinds = await DBService.collection("minds").get();
+
+        loadMinds.forEach(mind => {
+            const mindObj = {
+                ...mind.data(),
+                id: mind.id,
+            };
+
+            setMinds((prev) => [
+                mindObj,
+                ...prev
+            ]);
+        });
+    }
+
+    useEffect(() => {
+        getMinds();
+    }, []);
     const onChange = (e) => {
         const { target: { value } } = e;
 
@@ -26,6 +46,9 @@ const Home = ({ userObj }) => {
                 <input type="text" placeholder="What's your mind?" maxLength={120} onChange={onChange} value={mind} required />
                 <input type="submit" value="share" />
             </form>
+            <div>
+                {minds.map(mind => <div key={mind.id}>{mind.text}</div>)}
+            </div>
         </div>
     );
 }
