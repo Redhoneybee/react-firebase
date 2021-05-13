181a3592e563b16db4743fd8acd78b09c4b99a3a
coderB <flack319@gmail.com>
2021-05-13 11:26:06 +0900

    edit profile function

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { AuthService } from "fbase";
 import { useHistory } from "react-router";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+    const [displayName, setDisplayName] = useState(userObj.displayName);
+    const [error, setError] = useState("");
     const history = useHistory();
     const onSignOut = () => {
         try {
@@ -12,8 +14,38 @@ const Profile = () => {
             alert(error.message);
         }
     }
+
+
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        if (userObj.displayName !== displayName) {
+            setError("");
+
+            await userObj.updateProfile({
+                displayName: displayName
+            });
+
+        } else {
+            setError("this is same the current your display name");
+        }
+    }
+
+    const onChange = (e) => {
+        const { target: { value } } = e;
+
+        setDisplayName(value);
+    }
     return (
         <div>
+            <div>
+                <form onSubmit={onSubmit}>
+                    <input type="text" placeholder="Edit your display name" onChange={onChange} value={displayName} required />
+                    <input type="submit" value="change the display name" />
+                </form>
+                {error}
+            </div>
             <div>
                 <button onClick={onSignOut}>SignOut</button>
             </div>
